4bfcd4244498bdac9553f1920f553fb6819e25e3
= <lincon_martins@sicredi.com.br>
2024-03-05 13:45:02 -0300

    feat: Add impl to contract find all and exception handler

--- webflux/src/main/java/br/com/webflux/service/UserService.java
+++ webflux/src/main/java/br/com/webflux/service/UserService.java
@@ -4,9 +4,13 @@ import br.com.webflux.entity.User;
 import br.com.webflux.mapper.UserMapper;
 import br.com.webflux.model.request.UserRequest;
 import br.com.webflux.repository.UserRepository;
+import br.com.webflux.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,13 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id) {
-        return repository.findById(id);
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new ObjectNotFoundException(format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName()))
+                ));
+    }
+
+    public Flux<User> findAll() {
+        return repository.findAll();
     }
 }
